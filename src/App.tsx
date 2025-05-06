6d813b482a206f55cc0999ff666dbc645a931927
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2025-05-06 09:21:29 +0200

    Create hello world. Add local state and try out jsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [searchQuery, setSearchQuery] = useState<string>("");
+
+    const welcome = {
+        greeting: "Hello",
+        name: "World"
+    }
 
     return (
         <>
             <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+                <h1>{welcome.greeting} {welcome.name}!</h1>
+                <label htmlFor='search'>Search: </label>
+                <input id="search" type="text" placeholder="Enter text here" onChange={(e) => setSearchQuery(e.target.value)} />
+                <p>Search query state: {searchQuery}</p>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     )
 }
